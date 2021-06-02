ed851fc0fe53cf3abca39300277c2442272bad54
DenisSvinkovskij <1911.denis55@gmail.com>
2021-06-02 12:07:31 +0300

    add create todo feature

--- routes/TodosRouter.js
+++ routes/TodosRouter.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const {
+  validateCreateTodo,
+} = require('../validation/todosValidation');
+const todosController = require('../controllers/todosController');
+
 const guard = require('../helpers/Guard');
 
-router.get('/', guard, contactsController.listContacts);
+// router.get('/', guard, contactsController.listContacts);
 
-router.get('/:contactId', guard, contactsController.getById);
+// router.get('/:contactId', guard, contactsController.getById);
 
-router.post('/', guard, createContact, contactsController.addContact);
-
-router.delete('/:contactId', guard, contactsController.removeContact);
-
-router.patch(
-  '/:contactId',
-  guard,
-  updateContact,
-  contactsController.updateContact,
+router.post(
+  '/',
+  // guard,
+  validateCreateTodo,
+  todosController.createTodo,
 );
 
-router.patch(
-  '/:contactId/favorite',
-  guard,
-  updateStatus,
-  contactsController.updateStatusContact,
-);
+// router.delete('/:contactId', guard, contactsController.removeContact);
+
+// router.patch(
+//   '/:contactId',
+//   guard,
+//   updateContact,
+//   contactsController.updateContact,
+// );
+
+// router.patch(
+//   '/:contactId/favorite',
+//   guard,
+//   updateStatus,
+//   contactsController.updateStatusContact,
+// );
 
 module.exports = router;
